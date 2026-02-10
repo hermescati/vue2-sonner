9e6a810c8a313d95dffb5d3f39a81c102ca911c9
Hermes Cati <hermescati@Hermess-MacBook-Pro.local>
2026-02-10 10:53:37 +0100

    fix: correct building files

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import vue2 from '@vitejs/plugin-vue2'
+import dts from 'vite-plugin-dts'
 import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [
     vue2(),
+    dts({
+      include: 'src/packages',
+      rollupTypes: true,
+      outDir: 'dist',
+    }),
   ],
   resolve: {
     alias: {
